7a3e5241e52f430ff94414d51560ae253756058a
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-11-27 12:57:35 +0100

    Added string manipulation

--- day_001/main.py
+++ day_001/main.py
@@ -1,1 +1,5 @@
-print("Hello, World!")
+print("Hello, World!\nThis is the Day 1 of #100DaysOfCode")
+
+print("New lines can be created ...\n... using the '\\n' line feed")
+
+print("Strings can be concatenated" + " " + "with the '+' operator")
