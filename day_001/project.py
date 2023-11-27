052e434b9584c8789a289a8018fc01164da3bead
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-11-27 15:34:28 +0100

    Completed Day 1 of #100DaysOfCode

---
+++ day_001/project.py
@@ -0,0 +1,12 @@
+# Day 1 - Final Project 
+# Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's name of the city you grew up in?\n")
+
+pet_name = input("What's name of your first pet?\n")
+
+band_name = city_name + " " + pet_name
+
+print("Your band name could be " + band_name)
