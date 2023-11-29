bb8d36a663b881a50756298da24c53f310cdad64
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-11-29 18:11:35 +0100

    Started day 3!

---
+++ days_001_010/day_003/00_rollercoaster.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your height (cm)? "))
+
+if height > 120:
+    print("You can rider the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
