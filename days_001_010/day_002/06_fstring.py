09ccbff1c333d5974615072cd0cc5dc8ebb7dc47
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-11-29 14:55:27 +0100

    Added format model

--- days_001_010/day_002/06_fstring.py
+++ days_001_010/day_002/06_fstring.py
@@ -11,3 +11,7 @@ print("isWinning = " + str(isWinning))
 print(f"score = {score}")
 print(f"height = {height}")
 print(f"isWinning = {isWinning}")
+
+heigth_format = "{:.4f}".format(height)
+
+print(f"Height formatted = {heigth_format}")
