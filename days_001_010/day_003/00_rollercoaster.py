1d139ae67471b5cefc5c8e9b738f6a42c80ea7d7
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-11-29 22:25:00 +0100

    Fixed BMI calculus

--- days_001_010/day_003/00_rollercoaster.py
+++ days_001_010/day_003/00_rollercoaster.py
@@ -4,5 +4,14 @@ height = int(input("What is your height (cm)? "))
 
 if height > 120:
     print("You can rider the rollercoaster!")
+
+    age = int(input("What is your age? "))
+
+    if age < 12:
+        print("Please pay €5.")
+    elif age <= 18:
+        print("Please pay €7.")
+    else:
+        print("Please pay €12.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
