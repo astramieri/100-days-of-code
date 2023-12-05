4c0ce7b79780fc15bbb4189c04cdda9052492559
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-12-05 13:04:46 +0100

    Almost fixed Reeborg Maze

--- days_001_010/day_006/07_maze.py
+++ days_001_010/day_006/07_maze.py
@@ -27,12 +27,13 @@ def turn_right():
     turn_left()
 
 while not at_goal():
-    if front_is_clear():
-        move()
-        
-        if not wall_on_right():
-            turn_right()
+    if wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
     else:
-        turn_right()  
+        turn_right()
+        move()
     
     
