4474c0e06750dc26223fb8ca5b3d65a726998b92
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-12-08 13:00:04 +0100

    Need to complete.

--- days_001_010/day_007/04_solution.py
+++ days_001_010/day_007/04_solution.py
@@ -15,15 +15,26 @@ print(f"Secret word: {secret_word}")
 
 game_over = False # initialization
 
+lives = 6 
+
 while not game_over:
     guess = input("Guess a letter: ").lower()
 
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guess:
-            secret_word[i] = guess
+    if guess in chosen_word:
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                secret_word[i] = guess
+    else:
+        lives -= 1
 
     print(f"Secret word: {secret_word}")
+    print(f"Remaining lives: {lives}")
 
     if not "_" in secret_word:
         game_over = True
         print("You win!")
+
+    if lives == 0:
+        game_over = True
+        print("You lose.")
+
