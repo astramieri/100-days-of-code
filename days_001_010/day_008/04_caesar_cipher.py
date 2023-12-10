100ced2f47e94848b6565747782d683e3e2b3133
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-12-10 22:27:37 +0100

    Fixed the Caesar Cipher

--- days_001_010/day_008/04_caesar_cipher.py
+++ days_001_010/day_008/04_caesar_cipher.py
@@ -1,20 +1,33 @@
 # Caesar Cipher
-
 while True:
     action = input("Type 'encode' to encrypt, 'decode' to decrypt, or 'exit' to quit: ")
 
     if action == 'exit':
         break
-    else:
-        message = input("Type your input message: ").lower()
+    elif action in ["encode", "decode"]:
+        input_text = input("Input text: ").lower()
+        output_text = ""
 
-        shift = int(input("Type the shift number: "))
+        shift = int(input("Shift number: "))
 
         if action == 'decode':
-             shift = shift * -1
+             shift = shift * -1 
 
-        for i in range(0, len(message)):
-            pass # tofix
+        base = 26
+        offset = 96 # a=97
+
+        for i in range(0, len(input_text)):
+            position = ord(input_text[i]) - offset # a=1, b=2, .. z=26
+
+            if position in range(1, base+1):
+                position += shift
+                position %= base
+
+                output_text += chr(position + offset)
+            else:
+                output_text += input_text[i]
+
+        print(f"Ouput text: {output_text}")
                  
 
 
