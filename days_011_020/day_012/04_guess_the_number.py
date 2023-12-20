596af5c7aab14056fd70e72c2297ea7775970001
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-12-20 12:25:19 +0100

    Completed Day 12!

---
+++ days_011_020/day_012/04_guess_the_number.py
@@ -0,0 +1,44 @@
+# Number Guessing Game
+
+import random
+
+secret_number = random.randint(1, 100)
+
+attempts = 0
+
+is_game_over = False
+
+def make_a_guess():
+    global attempts
+    global is_game_over
+
+    print(f"You have {attempts} attempts remaining to guess the number.")
+
+    guess = int(input("Make a guess: "))
+
+    if guess == secret_number:
+        print(f"You win! 😎")
+        is_game_over = True
+    elif guess < secret_number:
+        print(f"Too low! 😤")
+        attempts -= 1
+    elif guess > secret_number:
+        print(f"Too high! 😤")
+        attempts -= 1
+
+    if attempts == 0:
+        print(f"You lose! 😭")
+        is_game_over = True
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+if difficulty == 'easy':
+    attempts = 10
+elif difficulty == 'hard':
+    attempts = 5
+
+while not is_game_over:
+    make_a_guess()
