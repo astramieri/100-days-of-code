2141579f7ec13c0005c4fbd50a2d2b35b6f23551
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-12-20 10:21:19 +0100

    Started Day 12

---
+++ days_011_020/day_012/scope.py
@@ -0,0 +1,26 @@
+# Scope
+
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside fuction: {enemies}")
+
+increase_enemies()
+print(f"enemies inside fuction: {enemies}")
+
+# Global Scope
+player_health = 10
+
+# Local Scope
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(potion_strength)
+        print(player_health)
+
+    drink_potion()
+
+game()
+# print(potion_strength)
+
