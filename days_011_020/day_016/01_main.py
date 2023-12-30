0190dfa0372f1b304a3086a823cac0b533f9139b
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-12-30 23:19:21 +0100

    Created .venv

---
+++ days_011_020/day_016/01_main.py
@@ -0,0 +1,12 @@
+#from turtle import *
+from turtle import Turtle, Screen
+ 
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+screen = Screen()
+print(screen.canvheight)
+screen.exitonclick()
---
+++ days_011_020/day_016/02_pypi.py
@@ -0,0 +1,4 @@
+# PyPI Python Package Index
+# https://pypi.org/
+
+import prettytable
