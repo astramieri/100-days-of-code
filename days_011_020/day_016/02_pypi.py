70a7f7126502660b21fc71fd2dff85c580d97297
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-01-01 22:51:50 +0100

    Refactoring the Coffee Machine

--- days_011_020/day_016/02_pypi.py
+++ days_011_020/day_016/02_pypi.py
@@ -1,4 +1,13 @@
 # PyPI Python Package Index
 # https://pypi.org/
 
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"], "l")
+table.add_column("Type", ["Eletric", "Water", "Fire"], "l")
+
+table.align = "r"
+
+print(table)
---
+++ days_011_020/day_016/03_coffee.py
@@ -0,0 +1,14 @@
+from module.menu import Menu, MenuItem
+from module.coffee_maker import CoffeeMaker
+from module.money_machine import MoneyMachine
+
+cm = CoffeeMaker()
+
+cm.report()
+
+menu = Menu()
+
+menu.get_items()
+
+
+
