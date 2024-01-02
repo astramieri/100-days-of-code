2ac57d6f3b9287788bb39f1cc229d561ecdbe11c
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-01-02 23:10:20 +0100

    Completed Day 16!

--- days_011_020/day_016/03_coffee.py
+++ days_011_020/day_016/03_coffee.py
@@ -1,14 +1,44 @@
+import os
+import art
+
 from module.menu import Menu, MenuItem
 from module.coffee_maker import CoffeeMaker
 from module.money_machine import MoneyMachine
 
-cm = CoffeeMaker()
 
-cm.report()
+def start_machine():
+    stop_machine = False
 
-menu = Menu()
+    os.system("cls")
+    print(art.logo)
 
-menu.get_items()
+    menu = Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+
+    while not stop_machine:
+        money_machine.report()
+        print(f"Resources: {coffee_maker.resources}")
+
+        choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+
+        if choice == "off":
+            print("Machine turned off.")
+            stop_machine = True
+        else:
+            menu_item = menu.find_drink(choice)
+
+            if menu_item is None:
+                print("")
+            else:
+                if not coffee_maker.is_resource_sufficient(menu_item):
+                    print("")
+                else:
+                    if not money_machine.make_payment(menu_item.cost):
+                        print("")
+                    else:
+                        coffee_maker.make_coffee(menu_item)
+                        print("")
 
 
-
+start_machine()
