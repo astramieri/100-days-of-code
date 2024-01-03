c0d09228aead8b9e89fc9e95f812d16c91afa7af
Angelo Stramieri <angelo.stramieri@gmail.com>
2024-01-03 22:20:16 +0100

    Added class functions aka methods

--- days_011_020/day_017/01_main.py
+++ days_011_020/day_017/01_main.py
@@ -2,7 +2,13 @@ class User:
     def __init__(self, user_id, user_name):
         self.user_id = user_id
         self.user_name = user_name
-        self.followers = 0 # default value
+        self.followers = 0  # default value
+        self.following = 0
+
+    def follow(self, user):
+        self.following += 1
+        user.followers += 1
+
 
 # user_1 = User()
 # user_1.user_id = "001"
@@ -11,4 +17,11 @@ class User:
 user_2 = User("001", "angelo")
 user_3 = User("002", "serena")
 
-print(f"followers: {user_2.followers}")
+user_2.follow(user_3)
+
+print(f"user_2.followers: {user_2.followers}")
+print(f"user_2.following: {user_2.following}")
+
+print(f"user_3.followers: {user_3.followers}")
+print(f"user_3.following: {user_3.following}")
+
